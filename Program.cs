5a799d1b4cc5837ce1ff6d6237fd0e32de2077f4
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-17 14:16:45 +0530

    Adding program for validating last name

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using RegexAssignment;
 
 Console.WriteLine("Wellcome to the user registration problem in regex");
 
+//UC1
 Console.WriteLine("Please Enter First Name");
 string pattern = Console.ReadLine();
 Patterns patterns = new Patterns();
@@ -13,3 +14,13 @@ if (val)
 else
     Console.WriteLine("Pattern Not Matched");
 
+
+//UC2
+Console.WriteLine("Please Enter Last Name");
+string lastname = Console.ReadLine();
+
+bool last = patterns.validateFirstName(lastname);
+if (last)
+    Console.WriteLine("Pattern Matched");
+else
+    Console.WriteLine("Pattern Not Matched");
