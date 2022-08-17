0b60d7d3ea1091c79b92a388351c3944e4220e94
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-17 14:13:21 +0530

    Proghra for Validating Last Name

---
+++ Patterns.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexAssignment
+{
+    internal class Patterns
+    {       
+        public static string RegexFirstName = "^[A-Z]{1,}[a-z]{2,}$";
+        public bool validateFirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, RegexFirstName);
+        }
+
+        public static string RegexLastName = "^[A-Z]{1,}[a-z]{3,}$";
+        public bool validateLastName(string LastName)
+        {
+            return Regex.IsMatch(LastName, RegexLastName);
+
+        }
+    }
+}
+
+
