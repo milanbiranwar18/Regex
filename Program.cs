a4238de5f01c3be1a62a043f51cc527d2d5ee43f
Milan Biranwar <milanbiranwar06@gmail.com>
2022-08-17 13:34:53 +0530

    Adding program for validating FirstName

--- Program.cs
+++ Program.cs
@@ -4,4 +4,12 @@ using RegexAssignment;
 
 Console.WriteLine("Wellcome to the user registration problem in regex");
 
+Console.WriteLine("Please Enter First Name");
+string pattern = Console.ReadLine();
+Patterns patterns = new Patterns();
+bool val = patterns.validateFirstName(pattern);
+if (val)
+    Console.WriteLine("Pattern Matched");
+else
+    Console.WriteLine("Pattern Not Matched");
 
